b5f5a2ea33853d36eb82fa2ef8ff02e5b190dacc
T-126 <rathnayaka3548@gmail.com>
2023-12-30 00:06:53 +0530

    chore: prettier-plugin added

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,9 @@
+import Image from "next/image";
+
 export default function Home() {
   return (
     <>
-      <h1 className="text-3xl text-gray-950">Hiii</h1>
+      <h1 className="text-4xl font-semibold">hi</h1>
     </>
   );
 }
