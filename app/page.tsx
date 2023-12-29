912cab7584478a4bca85c98295db1ce7b46ffe82
T-126 <rathnayaka3548@gmail.com>
2023-12-30 00:08:34 +0530

    style: minor changes

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <>
-      <h1 className="text-4xl font-semibold">hi</h1>
+      <h1 className="text-4xl font-semibold">Hi</h1>
     </>
   );
 }
